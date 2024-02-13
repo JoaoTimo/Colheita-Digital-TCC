29039e8da9874a25edd87492233e96c5f02ace0b
JoaoTimo <142836644+JoaoTimo@users.noreply.github.com>
2024-02-13 20:40:49 -0300

    Add files via upload

---
+++ ColheitaDigital/tcc_js.js
@@ -0,0 +1,24 @@
+function aoClicar1(){
+    window.location = "tcc_html.html"
+    return
+}
+
+function aoClicar2(){
+    window.location = "pagProdutor.html"
+    return
+}
+
+function aoClicar3(){
+    window.location = "pagComprador.html"
+    return
+}
+
+function aoClicar4(){
+    window.location = "sobreNos.html"
+    return
+}
+
+function aoClicar5(){
+    window.location = "beneficiosHTML.html"
+    return
+}
