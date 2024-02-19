196a01f5d513e67f1fec5763e6243bb7156b53d4
Felipe Domingos <131036732+Felelezin@users.noreply.github.com>
2024-02-18 21:40:22 -0300

    Add files via upload

---
+++ Parte Felipe/ColheitaDigital/tcc_js.js
@@ -0,0 +1,29 @@
+function aoClicar1(){
+    window.location = "tcc_html.html"
+    return
+}
+
+function aoClicar2(){
+    window.location = "pagProdutor.html"
+    return
+}
+
+function aoClicar3(){
+    window.location = "pagComprador.html"
+    return
+}
+
+function aoClicar4(){
+    window.location = "sobreNos.html"
+    return
+}
+
+function aoClicar5(){
+    window.location = "beneficiosHTML.html"
+    return
+}
+
+function aoClicar6(){
+    window.location = "pagLogin.html"
+    return
+}
